2f48590b7a1393e843b29d7e92a3e9615031fb8e
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-05-12 10:33:30 +0300

    Initial Commit

---
+++ NSNumberHelpersExamples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSNumberHelpersExamples
+//
+//  Created by Bogdan Stasjuk on 5/12/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSNumberHelpersExamplesTests/NSNumberHelpersExamplesTests.m
@@ -0,0 +1,34 @@
+//
+//  NSNumberHelpersExamplesTests.m
+//  NSNumberHelpersExamplesTests
+//
+//  Created by Bogdan Stasjuk on 5/12/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSNumberHelpersExamplesTests : XCTestCase
+
+@end
+
+@implementation NSNumberHelpersExamplesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
